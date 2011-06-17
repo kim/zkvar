1baf998598e98332090c2d28826e1e79f3a6974c
Kim Altintop <kim.altintop@gmail.com>
2011-06-17 09:14:56 +0200

    Hide zookeeper dependency

--- test.hs
+++ test.hs
@@ -1,9 +1,8 @@
 import Control.Monad (forever)
 import ZKVar
-import qualified Zookeeper.Core as ZK
 
 main = do
-  zk  <- ZK.init "127.0.0.1:2181" Nothing 10000 Nothing
+  zk  <- initZK "127.0.0.1:2181"
   zv1 <- newZKVar zk "/foo" "default foo"
   zv2 <- newZKVar zk "/bar" "default bar"
   forever $ do
